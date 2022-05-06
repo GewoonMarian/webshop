2d4076d64d21168ad7430d72505aa7d119ae7357
nextcode1 <stargateblk@gmail.com>
2022-05-06 10:46:42 +0200

    ShopProduct styled

--- src/pages/ShopProducts.js
+++ src/pages/ShopProducts.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { NavLink, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./ShopProduct.css";
 const URI = " http://localhost:4000/categories/";
 
@@ -32,18 +32,32 @@ export default function HouseDetails() {
             backgroundColor: "lightgray",
           }}
         >
-          <h1>{category.title}</h1>
+          <div className="tit-cat">
+            <h1>{category.title}</h1>
+          </div>
+
           <div className="details-prod">
             <div>
-              <img src={category.imgUrl} alt="" width={"600px"}></img>
-              <h2>Products</h2>
+              <div className="cat-imag">
+                <img src={category.imgUrl} alt="" width={"600px"}></img>
+              </div>
+              <div className="prod-tit">
+                <h2>Products</h2>
+              </div>
 
               {category.products.map((product) => {
                 return (
                   <p key={product.id}>
-                    <NavLink to={`/details/${product.id}`}>
-                      {product.title}
-                    </NavLink>
+                    <Link to={`/details/${product.id}`}>
+                      <div className="prod-img">
+                        {product.title}
+                        <img
+                          src={product.mainImage}
+                          width="200px"
+                          alt={product.name}
+                        />
+                      </div>
+                    </Link>
                   </p>
                 );
               })}
