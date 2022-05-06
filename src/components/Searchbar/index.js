d24551e6df75aa0ba1d813bdcb620c30cd85aafb
nextcode1 <stargateblk@gmail.com>
2022-05-06 11:44:01 +0200

    clean code up

--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import "./style.css";
 import { useEffect, useState } from "react";
-import ProductsCard from "../ProductCard";
-import Pagination from "../Pagination";
 import { Link } from "react-router-dom";
 
 // ?limit=5&offset=0
@@ -58,44 +56,6 @@ export default function Searchbar() {
           Hello
         </div>
       ) : null}
-
-      {/*       
-      {loading && <div>...loading</div>}
-      <div>
-        {!loading && products.length !== 0 ? (
-          products
-
-            .filter((product) => {
-              if (!product.title) return null;
-              return product.title.toLowerCase().includes(search.toLowerCase());
-            })
-            .map((p) => {
-              if (!p.title) return null;
-              return (
-                <ProductsCard
-                  key={p.id}
-                  title={p.title}
-                  price={p.price}
-                  rating={p.rating}
-                  imgUrl={p.mainImage}
-                  description={p.description}
-                  id={p.id}
-                />
-              );
-            })
-        ) : (
-          <div>No more items</div>
-        )}
-
-        <div>
-          <Pagination
-            offset={offset}
-            setOffset={setOffset}
-            productPerPage={productsPerPage}
-            amountOfProducts={products.length}
-          />
-        </div>
-      </div> */}
     </div>
   );
 }
